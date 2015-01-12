6e0cd5ddfef29f54eb3f70b7ab950f28750a90c6
UTEHN <UTEHN@UTEHN-NB>
2015-01-12 11:13:38 +0700

    2015-01-12,11.03

--- protected/config/main.php
+++ protected/config/main.php
@@ -8,6 +8,7 @@
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'ระบบรายงาน คปสอ.หนองหาน',
+        'timeZone'=>'Asia/Bangkok',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
