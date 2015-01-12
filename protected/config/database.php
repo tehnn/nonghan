68c895469d8c8ac6c239bf9bbd6c1bbdf03bdd33
UTEHN <UTEHN@UTEHN-NB>
2015-01-12 14:24:04 +0700

    2015-01-12

--- protected/config/database.php
+++ protected/config/database.php
@@ -4,11 +4,11 @@
 return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-	/*
-	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+	
+	'connectionString' => 'mysql:host=localhost;dbname=hos;port=3309',
 	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '',
+	'username' => 'sa',
+	'password' => 'sa',
 	'charset' => 'utf8',
-	*/
+	
 );
