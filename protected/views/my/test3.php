1e3ee992df0ecf18bca509ec0987b2965582c232
UTEHN <UTEHN@UTEHN-NB>
2015-01-12 13:53:03 +0700

    2015-01-12

--- protected/views/my/test3.php
+++ protected/views/my/test3.php
@@ -9,7 +9,14 @@ echo $date;
 <a href="<?php echo $this->createUrl('test3',array('no'=>'999999'));  ?>">link ไปที่ action ส่งค่าไปด้วย</a><br>
 
 <a href="<?php echo Yii::app()->createUrl('my/test3',array('no'=>'sdddddd'));  ?>">link ไปที่ action ไม่ส่งค่าไปด้วย</a> <br>
+<br>
+<?php
 
+   echo CHtml::link('ลิ้งอีกแบบ',array('my/test3',
+       'no'=>'aaaaaa'
+   ));
+    
+?>
 
 <hr>
 <?php
