219301981832bdfa1e14aac20c78e4b443c15dcd
UTEHN <UTEHN@UTEHN-NB>
2015-01-13 13:28:09 +0700

    2015-01-13

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -1,33 +1,50 @@
 <?php
 
-/**
- * UserIdentity represents the data needed to identity a user.
- * It contains the authentication method that checks if the provided
- * data can identity the user.
- */
-class UserIdentity extends CUserIdentity
-{
-	/**
-	 * Authenticates a user.
-	 * The example implementation makes sure if the username and password
-	 * are both 'demo'.
-	 * In practical applications, this should be changed to authenticate
-	 * against some persistent user identity storage (e.g. database).
-	 * @return boolean whether authentication succeeds.
-	 */
-	public function authenticate()
-	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else
-			$this->errorCode=self::ERROR_NONE;
-		return !$this->errorCode;
-	}
+class UserIdentity extends CUserIdentity {
+
+    const ERROR_USER_NOTAUTH = -1;
+
+    private $_id;
+
+    public function authenticate() {
+
+        $username = strtolower($this->username);
+        $user = User::model()->find('LOWER(username)=?', array($username));
+
+        if (empty($user)) {
+            $this->errorCode = self::ERROR_USERNAME_INVALID;
+        } else if ($user->password !== $this->password) {
+            $this->errorCode = self::ERROR_PASSWORD_INVALID;
+        } else if ($user->role === 'wait') {
+            $this->errorCode = self::ERROR_USER_NOTAUTH;
+        } else {
+
+            $this->_id = $user->username;
+            //$this->username = $user->username;
+            
+            $this->setState('prename', $user->prename);
+            $this->setState('fname', $user->fname);
+            $this->setState('type', $user->type);
+            $this->setState('role', $user->role);
+            $this->setState('company', $user->company);
+
+
+
+            $this->errorCode = self::ERROR_NONE;
+
+            //Update lastvisit
+            $u = User::model()->findByPK($this->_id);
+            $count = $u->count_login;
+            $date = date("Y-m-d H:i:s");
+            User::model()->updateByPk(array($this->_id), array('last_login' => $date));
+            User::model()->updateByPk(array($this->_id), array('count_login' => $count + 1));
+        }
+        
+        return !$this->errorCode;
+    }
+
+    public function getId() {
+        return $this->_id;
+    }
+
 }
